015fa692ef8d851e16796943c4959e606c1bed4c
student <student@N-4363-AS.elewise.local>
2020-06-23 14:01:07 +0400

    html

---
+++ frontend/index.js
@@ -0,0 +1,31 @@
+function test(input) {
+	let file = input.files[0];
+
+ 	let reader = new FileReader();
+
+ 	reader.readAsText(file);
+
+ 
+	reader.onload = function() {
+		var obj = JSON.parse(reader.result);
+    	console.log(obj["meetings"]);
+    	var r = obj["meetings"];
+    	console.log(r['0']);
+    	var d = r['0'];
+    	var currentUsers = d['currentUsers']
+    	document.getElementById('meetings').innerHTML = d["id"];
+
+    	for (var i = 0; i < d['currentUsers'].length; i++) {
+    		addUser(currentUsers[i])
+    	}
+  	};
+
+  	reader.onerror = function() {
+    console.log(reader.error);
+  	};
+}
+function addUser(id) {
+	var user = document.getElementById("1");
+	var cln = user.cloneNode(true);
+	document.getElementById("currentUsers").appendChild(cln);
+}
