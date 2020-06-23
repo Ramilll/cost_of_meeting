97ada68980053532de4e1a4e8b97573d70de9b7a
student <student@N-4363-AS.elewise.local>
2020-06-23 15:22:48 +0400

    reloading

--- frontend/index.js
+++ frontend/index.js
@@ -1,29 +1,29 @@
-function test(input) {
-	let file = input.files[0];
+// function test(input) {
+// 	let file = input.files[0];
 
- 	let reader = new FileReader();
+//  	let reader = new FileReader();
 
- 	reader.readAsText(file);
+//  	reader.readAsText(file);
 
  
-	reader.onload = function() {
-		var obj = JSON.parse(reader.result);
-    	console.log(obj["meetings"]);
-    	var r = obj["meetings"];
-    	console.log(r['0']);
-    	var d = r['0'];
-    	var currentUsers = d['currentUsers']
-    	document.getElementById('meetings').innerHTML = d["id"];
+// 	reader.onload = function() {
+// 		var obj = JSON.parse(reader.result);
+//     	console.log(obj["meetings"]);
+//     	var r = obj["meetings"];
+//     	console.log(r['0']);
+//     	var d = r['0'];
+//     	var currentUsers = d['currentUsers']
+//     	document.getElementById('meetings').innerHTML = d["id"];
 
-    	for (var i = 0; i < d['currentUsers'].length; i++) {
-    		addUser(currentUsers[i])
-    	}
-  	};
+//     	for (var i = 0; i < d['currentUsers'].length; i++) {
+//     		addUser(currentUsers[i])
+//     	}
+//   	};
 
-  	reader.onerror = function() {
-    console.log(reader.error);
-  	};
-}
+//   	reader.onerror = function() {
+//     console.log(reader.error);
+//   	};
+// }
 function addUser(id) {
 	var user = document.getElementById("1");
 	var cln = user.cloneNode(true);
